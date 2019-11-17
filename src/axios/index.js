d328d80fb34e5ee3ee6154ff6d6788a907e72eb1
Qiuhang817385 <826266129@qq.com>
2019-11-17 23:10:33 +0800

    dynamicRender

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,5 +1,8 @@
-import JsonP from 'jsonp'
+import JsonP from 'jsonp';
+import axios from 'axios';
+import { Modal } from 'antd'
 export default class Axios {
+    // 封装JSONP
     static jsonp(options) {
         return new Promise((resolve, reject) => {
             JsonP(options.url, {
@@ -13,5 +16,51 @@ export default class Axios {
                 }
             })
         })
+    }
+    // 封装API
+    static ajax(options) {
+        // 使用loading
+        let loading;
+        //如果有的接口不希望loading,做判断
+        if (options.data && options.data.isShowLoading !== false) {
+            loading = document.getElementById('ajaxLoading');
+            loading.style.display ='block'
+        }
+
+        return new Promise((resolve, reject) => {
+            // axios的api写法
+            axios({
+                url: options.url,
+                method: 'get',
+                baseURL: '',
+                timeout: 5000,
+                params: (options.data && options.data.params) || '',
+
+            }).then((res) => {
+
+
+                if (options.data && options.data.isShowLoading !== false) {
+                    loading = document.getElementById('ajaxLoading');
+                    loading.style.display ='none'
+                }
+
+
+                if (res.status === 200) {
+                    let response = res.data;
+                    if (response.code === 0) {
+                        // resolve(response.result)
+                        resolve(response)
+                    } else {
+                        Modal.info({
+                            title: '提示',
+                            content: response.msg
+                        })
+                    }
+                } else {
+                    reject(res.data);
+                }
+            })
+        });
+
     }
 }
--- src/page/table/basicTable.js
+++ src/page/table/basicTable.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Card, Table } from 'antd'
+import { Card, Table } from 'antd';
+// import axios from 'axios';
+import axios from '../../axios';  //封装的方式
 export default class BasicTable extends Component {
-    state={
-        dataSource2:[]
+    state = {
+        dataSource2: []
     }
     //  state = {
     //     y:4827
@@ -18,33 +20,76 @@ export default class BasicTable extends Component {
                     2.外部 直接赋值
                     3.内部const
      */
-    componentDidMount () {
+    componentDidMount() {
         const dataSource = [
             {
-                id:'0',
-                userName:'qiu',
-                sex:'1',
-                state:'1',
-                interest:'1',
-                birthday:'2000-01-01',
-                address:'秦皇岛海港区',
-                time:'09:00'
+                id: '0',
+                userName: 'qiu',
+                sex: '1',
+                state: '1',
+                interest: '1',
+                birthday: '2000-01-01',
+                address: '秦皇岛海港区',
+                time: '09:00'
             },
             {
-                id:'1',
-                userName:'Hang',
-                sex:'1',
-                state:'1',
-                interest:'1',
-                birthday:'2000-01-01',
-                address:'秦皇岛海港区',
-                time:'09:00'
+                id: '1',
+                userName: 'Hang',
+                sex: '1',
+                state: '1',
+                interest: '1',
+                birthday: '2000-01-01',
+                address: '秦皇岛海港区',
+                time: '09:00'
             }
         ]
         // 这步的作用是存储DataSource
         this.setState({
             dataSource
         })
+        this.request();
+    }
+    request = () => {
+      /*   axios.get('/api/tableList')
+            .then((res) => {
+                if (res.status === 200 && res.data.code === 0) {
+                    let result = res.data.result;
+                    // result是一个数组
+                    // this.setState(()=>{
+                    //     return {
+                    //         dataSource2:result
+                    //     }
+                    // })
+                    this.setState(() => ({ dataSource2: [...result] }))
+                    // for (const key in result) {
+                    //    console.log(result[key]);
+                    // }
+                    console.log(result);
+                    console.log(typeof res.status);
+                }
+            })
+            .catch(() => { alert('error') }) 
+            
+            使用直接使用的方式
+            */
+           /* 
+           使用封装的方式
+
+            */
+           axios.ajax({
+               url:'/api/tableList',
+               data:{
+                   params:{
+                    //    tableList?page=1
+                       page:1
+                   },
+                //    isShowLoading:false
+               }
+           }).then((res)=>{
+                if(res.code===0){
+                    this.setState(() => ({ dataSource2: [...res.result] }))
+                }
+           })
     }
     render() {
         // 变量定义在外面和定义在里面其实一样,需要引用的方式不一样
@@ -60,10 +105,23 @@ export default class BasicTable extends Component {
                 dataIndex: 'userName'
             }, {
                 title: '性别',
-                dataIndex: 'sex'
+                dataIndex: 'sex',
+                render(sex){
+                    return sex===1?'男':'女'
+                }
             }, {
                 title: '状态',
-                dataIndex: 'state'
+                dataIndex: 'state',
+                render(state){
+                    let config ={
+                        '1':'这是1',
+                        '2':'zheshi2',
+                        '3':'这是3',
+                        '4':'这是4',
+                        '5':'这是5'
+                    }
+                    return config[state];
+                }
             }, {
                 title: '爱好',
                 dataIndex: 'interest'
@@ -99,7 +157,9 @@ export default class BasicTable extends Component {
                     </Table>
                 </Card>
 
+                <Card title="data">
 
+                </Card>
             </div>
         )
     }
